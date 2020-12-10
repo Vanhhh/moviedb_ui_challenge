0ffefc4b7251c3ef6e7c4adb170df9c4c659ea70
Viet Anh Ly <39008333+Vanhhh@users.noreply.github.com>
2020-12-10 14:00:09 +0100

    verbesserung

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import { TMDB_API_KEY } from "../api/key";
 import Movie from "./Movie";
-
+import styled from "styled-components";
 import { Search } from "react-feather";
 
 export default function App() {
@@ -45,38 +45,110 @@ export default function App() {
     event.preventDefault();
   };
 
+  ////styled-components////
+
+  ///colors////
+  const colorblue = "#28a5ff";
+  const colorwhitegray = "#f4f4f4";
+
+  const MovieList = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    justify-content: center;
+    grid-column-gap: 7%;
+    margin-top: 4rem;
+    width: 80%;
+    margin: 4rem auto 0 auto;
+  `;
+  const Movie = styled.div`
+    margin-bottom: 3rem;
+    overflow: hidden;
+  `;
+
+  const WebTitle = styled.h1`
+    font-weight: bold;
+    font-size: 4rem;
+    line-height: 4rem;
+    margin: 4.8rem;
+  `;
+  const TitleMovie = styled.span`
+    color: ${(props) => (props.title__movie = colorblue)};
+  `;
+  const SearchInput = styled.input`
+    background: ${(props) => (props.search__input = colorwhitegray)};
+    font-size: 1.8rem;
+    line-height: 2.3rem;
+    width: 35rem;
+    height: 5.6rem;
+    left: 0rem;
+    top: calc(50% - 56rem / 2);
+    border: none;
+    border-radius: 0.5rem;
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+    margin-right: 0.8rem;
+    cursor: pointer;
+    padding: 1.65rem 0;
+    padding-left: 1.6rem;
+    box-sizing: border-box;
+    transition: all 0.2s;
+  `;
+  const SearchSubMit = styled.button`
+    background: ${(props) => (props.search__submit = colorblue)};
+    width: 5.6rem;
+    height: 5.6rem;
+    left: 35.8rem;
+    top: 0rem;
+    border-radius: 0.5rem;
+    border: none;
+    cursor: pointer;
+  `;
+  const Form = styled.form`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `;
+  const App = styled.div`
+    text-align: center;
+    font-family: "IBM Plex Sans", sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 2rem;
+  `;
+
   const movieListHtmlElement =
     data && data.length ? (
-      <div className="movieList">
+      <MovieList className="movieList">
         {data.map((item) => (
           <Movie key={item.id} movie={item} className="movie" />
         ))}
-      </div>
+      </MovieList>
     ) : (
       <span></span>
     );
 
   return (
-    <div className="App">
-      <h1 className="web__title">
-        <span id="title__movie">Movie</span>
+    <App className="app">
+      <WebTitle className="web__title">
+        <TitleMovie id="title__movie">Movie</TitleMovie>
         <span>DB</span>
-      </h1>
-      <form className="form" onSubmit={onSubmit}>
-        <input
+      </WebTitle>
+      <Form className="form" onSubmit={onSubmit}>
+        <SearchInput
           type="text"
           name="search"
           placeholder="Type in search query..."
-          className="search"
+          className="search__input"
           value={queryText}
           ref={searchInput}
           onChange={(e) => setQueryText(e.value)}
         />
-        <button className="search-submit">
+        <SearchSubMit className="search__submit">
           <Search color="white" />
-        </button>
-      </form>
+        </SearchSubMit>
+      </Form>
       {loading ? <p>Loading...</p> : movieListHtmlElement}
-    </div>
+    </App>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,22 +1,70 @@
 import React from "react";
 import moment from "moment";
+import styled from "styled-components";
+
+////styled-components////
+
+///colors////
+const colorblue = "#28a5ff";
+const colorwhite = "#ffffff";
+const MovieImg = styled.div`
+  height: 27.27vw;
+  border-radius: 0.5rem;
+  width: 100%;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  transition: all 0.5s;
+`;
+
+const MovieVote = styled.span`
+  background: ${(props) => (props.movie__vote = colorblue)};
+  border-radius: 0.5rem;
+  color: ${(props) => (props.movie__vote = colorwhite)};
+  font-size: 2rem;
+  line-height: 2.6rem;
+  padding: 1.1rem 0.9rem;
+  position: absolute;
+  z-index: 1;
+  bottom: 1rem;
+  right: 1rem;
+`;
+
+const MovieBottomContainer = styled.div`
+  text-align: left;
+  margin-top: 1.6rem;
+`;
+const MovieTitle = styled.h3`
+  font-weight: 700;
+  font-size: 2rem;
+  line-height: 2.6rem;
+  margin-bottom: 0.3rem;
+`;
+const MovieDate = styled.h4`
+  font-weight: 400;
+  font-size: 1.4rem;
+  color: $color-gray;
+  line-height: 1.8rem;
+`;
 
 const Movie = ({ movie, ...rest }) => {
   console.log(movie);
   return (
     <div {...rest}>
-      <img
+      <MovieImg
         className="movie__img"
-        src={`//image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`}
-        alt={movie.title + " - Poster"}
-      />
-      <div className="movie__bottom-container">
-        <h3 className="movie__title">{movie.title}</h3>
-        <h4 className="movie__date">
+        style={{
+          backgroundImage: `url(//image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path})`,
+        }}
+      >
+        <MovieVote className="movie__vote">{movie.vote_average}</MovieVote>
+      </MovieImg>
+      <MovieBottomContainer className="movie__bottom__container">
+        <MovieTitle className="movie__title">{movie.title}</MovieTitle>
+        <MovieDate className="movie__date">
           {moment(movie.release_date).format("DD.MM.YYYY")}
-        </h4>
-      </div>
-      <span className="movie__vote">{movie.vote_average}</span>
+        </MovieDate>
+      </MovieBottomContainer>
     </div>
   );
 };
